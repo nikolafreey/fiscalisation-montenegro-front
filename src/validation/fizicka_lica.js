1b20e958e048c7a5a30dc8928736ad4680d7d75f
Andrej Jokic <aj.jokic@gmail.com>
2020-11-28 16:11:39 +0100

    Validacija za fizicka lica

--- src/validation/fizicka_lica.js
+++ src/validation/fizicka_lica.js
@@ -6,52 +6,35 @@ export const FizickaLicaSchema = Yup.object().shape({
     .min(3, $t('validacija.unosKratak'))
     .max(50, $t('validacija.unosDugacak'))
     .required($t('validacija.obavezno')),
-  /*prezime: Yup.string()
+  prezime: Yup.string()
     .min(3, $t('validacija.unosKratak'))
     .max(50, $t('validacija.unosDugacak'))
     .required($t('validacija.obavezno')),
   jmbg: Yup.string()
-    .min(13, $t('validacija.unosKratak'))
-    .max(13, $t('validacija.unosDugacak'))
+    .length(13, $t('validacija.duzina'))
     .required($t('validacija.obavezno')),
   ib: Yup.string()
-    .min(8, $t('validacija.unosKratak'))
-    .max(8, $t('validacija.unosDugacak'))
-    .required($t('validacija.obavezno')),
+    .length(8, $t('validacija.duzina')),
   adresa: Yup.string()
-    .min(3, $t('validacija.unosKratak'))
-    .max(50, $t('validacija.unosDugacak'))
-    .required($t('validacija.obavezno')),
+    .max(191, $t('validacija.unosDugacak')),
   telefon: Yup.string()
-    .min(3, $t('validacija.unosKratak'))
-    .max(50, $t('validacija.unosDugacak'))
-    .required($t('validacija.obavezno')),
+    .max(191, $t('validacija.unosDugacak')),
   email: Yup.string()
-    .min(3, $t('validacija.unosKratak'))
-    .max(50, $t('validacija.unosDugacak'))
+    .email($t('validacija.email'))
+    .max(191, $t('validacija.unosDugacak'))
     .required($t('validacija.obavezno')),
   zanimanje: Yup.string()
     .min(3, $t('validacija.unosKratak'))
-    .max(50, $t('validacija.unosDugacak'))
-    .required($t('validacija.obavezno')),
+    .max(50, $t('validacija.unosDugacak')),
   radno_mjesto: Yup.string()
-    .min(3, $t('validacija.unosKratak'))
-    .max(50, $t('validacija.unosDugacak'))
-    .required($t('validacija.obavezno')),
+    .max(50, $t('validacija.unosDugacak')),
   drzavljanstvo: Yup.string()
-    .min(3, $t('validacija.unosKratak'))
-    .max(50, $t('validacija.unosDugacak'))
-    .required($t('validacija.obavezno')),
+    .max(50, $t('validacija.unosDugacak')),
   nacionalnost: Yup.string()
-    .min(3, $t('validacija.unosKratak'))
-    .max(50, $t('validacija.unosDugacak'))
-    .required($t('validacija.obavezno')),
+    .max(50, $t('validacija.unosDugacak')),
   cv_link: Yup.string()
-    .min(3, $t('validacija.unosKratak'))
-    .max(50, $t('validacija.unosDugacak'))
-    .required($t('validacija.obavezno')),
+    .url($t('validacija.url'))
+    .max(255, $t('validacija.unosDugacak')),
   avatar: Yup.string()
-    .min(3, $t('validacija.unosKratak'))
-    .max(50, $t('validacija.unosDugacak'))
-    .required($t('validacija.obavezno')),*/
+    .max(255, $t('validacija.unosDugacak')),
 });
