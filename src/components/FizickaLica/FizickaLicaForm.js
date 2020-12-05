c00f07d27fb26280718967484125b0a78b9c40de
Željko Janjic <zeljko.janjic@payspotns.local>
2020-12-06 00:43:19 +0100

    RCN 77

--- src/components/FizickaLica/FizickaLicaForm.js
+++ src/components/FizickaLica/FizickaLicaForm.js
@@ -1,9 +1,16 @@
 import { FieldArray, Form, Formik } from 'formik';
 import React, { useEffect } from 'react';
 import { FizickaLicaSchema } from '../../validation/fizicka_lica';
+import { ReactComponent as Link } from '../../assets/icon/link.svg';
+
 import $t from '../../lang';
 import { useDispatch, useSelector } from 'react-redux';
-import { deleteFizickoLice, getFizickoLice, storeFizickoLice, updateFizickoLice } from '../../store/actions/FizickaLicaActions';
+import {
+  deleteFizickoLice,
+  getFizickoLice,
+  storeFizickoLice,
+  updateFizickoLice,
+} from '../../store/actions/FizickaLicaActions';
 import DropDown from '../shared/forms/DropDown';
 import InputField from '../shared/forms/InputField';
 import { useRouteMatch } from 'react-router-dom';
@@ -23,98 +30,278 @@ const FizickaLicaForm = () => {
   }, [dispatch, params]);
 
   const handleSubmit = (values) => {
-    if (params.id) dispatch(updateFizickoLice({id: params.id, ...values}));
+    if (params.id) dispatch(updateFizickoLice({ id: params.id, ...values }));
     else dispatch(storeFizickoLice(values));
-  }
+  };
 
   return (
     <Formik
       initialValues={{
-        'ime': '',
-        'prezime': '',
-        'jmbg': '',
-        'ib': '',
-        'adresa': '',
-        'telefon': '',
-        'email': '',
-        'zanimanje': '',
-        'radno_mjesto': '',
-        'drzavljanstvo': '',
-        'nacionalnost': '',
-        'cv_link': '',
-        'avatar': '',
-        'preduzece_id': '',
-        'ziro_racuni': [],
-        ...fizickoLice
+        ime: '',
+        prezime: '',
+        jmbg: '',
+        ib: '',
+        adresa: '',
+        grad: '',
+        drzava: '',
+
+        telefon: '',
+        email: '',
+        zanimanje: '',
+        radno_mjesto: '',
+        drzavljanstvo: '',
+        nacionalnost: '',
+        cv_link: '',
+        avatar: '',
+        preduzece_id: '',
+        ziro_racuni: [],
+        ...fizickoLice,
       }}
       onSubmit={handleSubmit}
       validationSchema={FizickaLicaSchema}
       enableReinitialize
     >
-      {({values}) => (<Form>
-        <InputField
-          name="ime"
-          label={$t('fizickalica.ime')}
-        />
-        <InputField
-          name="prezime"
-          label={$t('fizickalica.prezime')}
-        />{' '}
-        <InputField
-          name="jmbg"
-          label={$t('fizickalica.jmbg')}
-        />
-        <InputField
-          name="ib"
-          label={$t('fizickalica.ib')}
-        />
-        <InputField
-          name="adresa"
-          label={$t('fizickalica.adresa')}
-        />
-        <InputField
-          name="telefon"
-          label={$t('fizickalica.telefon')}
-        />
-        <InputField
-          name="email"
-          label={$t('fizickalica.email')}
-        />
-        <InputField
-          name="zanimanje"
-          label={$t('fizickalica.zanimanje')}
-        />
-        <InputField
-          name="radno_mjesto"
-          label={$t('fizickalica.radno_mjesto')}
-        />
-        <InputField
-          name="drzavljanstvo"
-          label={$t('fizickalica.drzavljanstvo')}
-        />
-        <InputField
-          name="nacionalnost"
-          label={$t('fizickalica.nacionalnost')}
-        />
-        <InputField
-          name="cv_link"
-          label={$t('fizickalica.cv_link')}
-        />
-        <InputField
-          name="avatar"
-          label={$t('fizickalica.avatar')}
-        />
-        <DropDown
-          name="preduzece_id"
-          label={$t('fizickalica.preduzece_id')}
-          loadOptions={preduzecaService.getPreduzecaDropdown}
-        />
-        <FieldArray name='ziro_racuni'>
-          {(arrayHelpers) => <ZiroRacuniFieldArray {...arrayHelpers}/>}
-        </FieldArray>
-        <button type="submit">Submit</button>
-        <button type="button" onClick={() => dispatch(deleteFizickoLice(params.id))}>Delete</button>
-      </Form>)}
+      {({ values }) => (
+        <div className="screen-content">
+          <a href="#" class="link df">
+            <Link />
+            <p>Povratak na Preduzeća</p>
+          </a>
+
+          <h1 class="heading-primary">Dodavanje novog fizičkog lica</h1>
+          <div className="main-content__box">
+            <div className="content">
+              <Form>
+                <div className="container">
+                  <div className="row">
+                    <div className="col-md-4 mt-25">
+                      <h2 className="heading-secondary">Informacije</h2>
+                      <p>
+                        Consequat eget volutpat enim libero nulla neque
+                        ultrices. Sed tristique nullam erat in interdum.
+                      </p>
+                    </div>
+                    <div className="col-md-8 mtb-25">
+                      <div className="df jc-sb">
+                        <div className="form__group w-48">
+                          <InputField
+                            name="ime"
+                            className="form__input"
+                            label={$t('fizickalica.ime')}
+                          />
+                        </div>
+                        <div className="form__group w-48">
+                          <InputField
+                            name="prezime"
+                            className="form__input"
+                            label={$t('fizickalica.prezime')}
+                          />
+                        </div>
+                      </div>
+                      <div className="form__group">
+                        <InputField
+                          name="zanimanje"
+                          label={$t('fizickalica.zanimanje')}
+                          className="form__input w-100"
+                        />
+                      </div>
+                      <div className="df jc-sb">
+                        <div className="form__group w-48">
+                          <InputField
+                            name="jmbg"
+                            className="form__input"
+                            label={$t('fizickalica.jmbg')}
+                          />
+                        </div>
+                        <div className="form__group w-48">
+                          <InputField
+                            name="ib"
+                            className="form__input"
+                            label={$t('fizickalica.ib')}
+                          />
+                        </div>
+                      </div>
+                      <div className="form__group">
+                        <InputField
+                          name="adresa"
+                          className="form__input w-100"
+                          label={$t('fizickalica.adresa')}
+                        />
+                      </div>
+                      <div className="df jc-sb">
+                        <div className="form__group w-48">
+                          <InputField
+                            name="grad"
+                            className="form__input"
+                            label={$t('fizickalica.grad')}
+                          />
+                        </div>
+                        <div className="form__group w-48">
+                          <InputField
+                            name="drzava"
+                            className="form__input"
+                            label={$t('fizickalica.drzava')}
+                          />
+                        </div>
+                      </div>
+
+                      <div className="df jc-sb">
+                        <div className="form__group w-48">
+                          <InputField
+                            name="nacionalnost"
+                            className="form__input"
+                            label={$t('fizickalica.nacionalnost')}
+                          />
+                        </div>
+                        <div className="form__group w-48">
+                          <InputField
+                            className="form__input"
+                            name="drzavljanstvo"
+                            label={$t('fizickalica.drzavljanstvo')}
+                          />
+                        </div>
+                      </div>
+
+                      <div className="df jc-sb">
+                        <div className="form__group w-48">
+                          <DropDown
+                            name="preduzece_id"
+                            className="form__input"
+                            label={$t('fizickalica.preduzece_id')}
+                            loadOptions={preduzecaService.getPreduzecaDropdown}
+                          />
+                        </div>
+                        <div className="form__group w-48">
+                          <InputField
+                            name="radno_mjesto"
+                            className="form__input"
+                            label={$t('fizickalica.radno_mjesto')}
+                          />
+                        </div>
+                      </div>
+                      <div className="form__group">
+                        <label className="form__label" for="">
+                          Opis
+                        </label>
+                        <textarea
+                          name=""
+                          id=""
+                          cols="30"
+                          rows="5"
+                          className="form__input"
+                        ></textarea>
+                      </div>
+                      <div className="form__group form__area">
+                        <InputField
+                          className="form__input w-100"
+                          name="avatar"
+                          label={$t('fizickalica.avatar')}
+                        />
+
+                        <div className="file-dummy">
+                          <div className="success">
+                            Uspješno ste selektovali fajl!
+                          </div>
+                          <div className="default">
+                            Prevucite fotografiju ovdje
+                            <br />
+                            ili kliknite da dodate
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <hr />
+
+                <div className="container">
+                  <div className="row">
+                    <div className="col-md-4">
+                      <h2 className="heading-secondary">Tekući računi</h2>
+                      <p>
+                        Consequat eget volutpat enim libero nulla neque
+                        ultrices. Sed tristique nullam erat in interdum.
+                      </p>
+                    </div>
+                    <div className="col-md-8">
+                      <FieldArray name="ziro_racuni">
+                        {(arrayHelpers) => (
+                          <ZiroRacuniFieldArray {...arrayHelpers} />
+                        )}
+                      </FieldArray>
+                    </div>
+                  </div>
+                </div>
+                <hr />
+                <div className="container">
+                  <div className="row">
+                    <div className="col-md-4">
+                      <h2 className="heading-secondary">Kontakt informacije</h2>
+                      <p>
+                        Consequat eget volutpat enim libero nulla neque
+                        ultrices. Sed tristique nullam erat in interdum.
+                      </p>
+                    </div>
+                    <div className="col-md-8">
+                      <div className="df jc-sb mb-20">
+                        <div className="form__group w-100">
+                          <InputField
+                            name="telefon"
+                            className="form__input w-100"
+                            label={$t('fizickalica.telefon')}
+                          />
+                        </div>
+                      </div>
+                      <div class="df fd-column">
+                        <div class="form__group w-100">
+                          <InputField
+                            className="form__input w-100"
+                            name="email"
+                            label={$t('fizickalica.email')}
+                          />
+                        </div>
+
+                        <div class="form__group w-100">
+                          <InputField
+                            className="form__input w-100"
+                            name="cv_link"
+                            label={$t('fizickalica.cv_link')}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <hr />
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md-4">
+                      <h2 class="heading-secondary">Status</h2>
+                      <p>
+                        Consequat eget volutpat enim libero nulla neque
+                        ultrices. Sed tristique nullam erat in interdum.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="form__footer">
+                  <button class="btn btn__dark btn__sm" type="submit">
+                    Sačuvaj
+                  </button>
+                  <button class="btn btn__link ml-m">Nazad</button>
+
+                  {/* <button
+                    type="button"
+                    onClick={() => dispatch(deleteFizickoLice(params.id))}
+                  >
+                    Delete
+                  </button> */}
+                </div>
+              </Form>
+            </div>
+          </div>
+        </div>
+      )}
     </Formik>
   );
 };
--- src/components/FizickaLica/ZiroRacuniFieldArray.js
+++ src/components/FizickaLica/ZiroRacuniFieldArray.js
@@ -1,24 +1,47 @@
-import { useFormikContext } from 'formik'
-import React from 'react'
+import { useFormikContext } from 'formik';
+import React from 'react';
 import InputField from '../shared/forms/InputField';
 import $t from '../../lang';
+import { ReactComponent as Plus } from '../../assets/icon/plus.svg';
 
 const ZiroRacuniFieldArray = ({ insert, remove }) => {
-  const { values } = useFormikContext()
-  
+  const { values } = useFormikContext();
+
   return (
     <div>
-      {
-        values.ziro_racuni.map((ziro_racun, index) => (
-          <>
-            <InputField name={`ziro_racuni.${index}.broj_racuna`}/>
-            <button type='button' onClick={() => remove(index)}>{$t('common.izbrisi')}</button>
-          </>
-        ))
-      }
-      <button type='button' onClick={() => insert(values.ziro_racuni.length, '')}>{$t('common.dodajNovi')}</button>
+      <label className="form__label" for="lista-racuna">
+        Lista računa
+      </label>
+
+      {values.ziro_racuni.map((ziro_racun, index) => (
+        <>
+          <div className="pr df ai-c">
+            <InputField
+              className="form__input w-50 mb-20"
+              name={`ziro_racuni.${index}.broj_racuna`}
+            />
+            <button
+              className="btn btn__link warning"
+              type="button"
+              onClick={() => remove(index)}
+            >
+              {$t('common.izbrisi')}
+            </button>
+          </div>
+        </>
+      ))}
+      <div class="df ai-c">
+        <button
+          type="button"
+          class="btn btn__link df"
+          onClick={() => insert(values.ziro_racuni.length, '')}
+        >
+          <Plus class="icon icon__stroke-link sm" />
+          <span class="btn btn__link success">{$t('common.dodajNovi')}</span>
+        </button>
+      </div>
     </div>
   );
-}
+};
 
-export default ZiroRacuniFieldArray
+export default ZiroRacuniFieldArray;
