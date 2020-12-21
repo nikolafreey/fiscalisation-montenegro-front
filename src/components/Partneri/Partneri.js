a9d364a7a9cce384880e18a9bc329220f47bf9a4
Željko Janjic <zeljko.janjic@payspotns.local>
2020-12-21 23:45:24 +0100

    Partneri dizajn

--- src/components/Partneri/Partneri.js
+++ src/components/Partneri/Partneri.js
@@ -9,6 +9,12 @@ import PartneriTable from './PartneriTable';
 import PreduzeceDetails from '../Preduzeca/PreduzeceDetails';
 import FizickoLiceDetails from '../FizickaLica/FizickoLiceDetails';
 import { debounce } from 'lodash';
+import { ReactComponent as PlusLightSvg } from '../../assets/icon/plusLight.svg';
+
+import { ReactComponent as PreduzeceSvg } from '../../assets/icon/hero-preduzecaDropdown.svg';
+import { ReactComponent as UserSvg } from '../../assets/icon/user.svg';
+import { Link } from 'react-router-dom';
+import { FIZICKA_LICA, PREDUZECA } from '../../constants/routes';
 
 const searchDebounced = debounce((callback) => callback(), 500);
 
@@ -30,7 +36,7 @@ const Partneri = () => {
   }, [dispatch, filter]);
 
   useEffect(() => {
-    if(search === '') filters.search = null;
+    if (search === '') filters.search = null;
     else filters.search = search;
     searchDebounced(getFiltered);
   }, [dispatch, search]);
@@ -45,7 +51,59 @@ const Partneri = () => {
 
   return (
     <>
-      <h1 className="heading-primary">Partneri</h1>
+      <div class="screen-content-info">
+        <div class="title">
+          <h1 class="heading-primary">Partneri</h1>
+          <button class="btn btn__dark btn__xl">
+            <PlusLightSvg />
+            Novi partner
+            <div class="drop-down" id="ddl">
+              <Link to={PREDUZECA.CREATE}>
+                <PreduzeceSvg />
+                Preduzeće
+              </Link>
+              <Link to={FIZICKA_LICA.CREATE}>
+                <UserSvg />
+                Fizičko lice
+              </Link>
+            </div>
+          </button>
+        </div>
+        <div className="main-content__box">
+          <div className="content">
+            <div className="main-content__search-wrapper df">
+              <form className="search df ai-c w-60">
+                <button className="search__button"></button>
+                <input
+                  type="text"
+                  className="search__input"
+                  placeholder="Naziv ili PIB preduzeca"
+                  value={search}
+                  onChange={(event) => setSearch(event.target.value)}
+                />
+              </form>
+              <select
+                className="btn btn__dark btn__lg ml-xl"
+                value={filter}
+                onChange={(event) => setFilter(event.target.value)}
+              >
+                <option value={'sve'}>Sve</option>
+                <option value={'fizicko_lice'}>Fizička lica</option>
+                <option value={'preduzece'}>Preduzeća</option>
+              </select>
+            </div>
+            <PartneriTable partneri={partneri} />
+          </div>
+          {partner.preduzece && (
+            <PreduzeceDetails preduzece={partner.preduzece} />
+          )}
+          {partner.fizicko_lice && (
+            <FizickoLiceDetails fizickoLice={partner.fizicko_lice} />
+          )}
+        </div>
+      </div>
+
+      {/* <h1 className="heading-primary">Partneri</h1>
       <div className="main-content__box">
         <div className="content">
           <div className="main-content__search-wrapper df">
@@ -73,7 +131,7 @@ const Partneri = () => {
         {partner.fizicko_lice && (
           <FizickoLiceDetails fizickoLice={partner.fizicko_lice} />
         )}
-      </div>
+      </div> */}
     </>
   );
 };
