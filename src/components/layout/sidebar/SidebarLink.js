562a4391f3fa9cab85d9acaf5ac403c66927e2fc
Nikola Vukovic <nikolav54@gmail.com>
2021-01-23 14:29:37 +0100

    Sidebar Router dodata klasa za aktivni link

--- src/components/layout/sidebar/SidebarLink.js
+++ src/components/layout/sidebar/SidebarLink.js
@@ -1,16 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
-const SidebarLink = ({label, ImageSource, num, ...props}) => {
+const SidebarLink = ({ label, ImageSource, num, ...props }) => {
+  const location = useLocation();
+  console.log('props SidebarLink:', props);
+
   return (
-    <Link {...props} >
-    <li className="item">
-      <ImageSource stroke="currentColor" className="sidebar-icon mr-m"/>
-      <p className="link" {...props}>{label}</p>
-      {num && <span className="">{num}</span>}
-    </li>
+    <Link {...props}>
+      <li className={props.to === location.pathname ? 'item active' : 'item'}>
+        <ImageSource stroke="currentColor" className="sidebar-icon mr-m" />
+        <p className="link" {...props}>
+          {label}
+        </p>
+        {num && <span className="">{num}</span>}
+      </li>
     </Link>
-  )
-}
+  );
+};
 
-export default SidebarLink
+export default SidebarLink;
