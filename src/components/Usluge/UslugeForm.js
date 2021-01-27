bb646d80fafac8b5b45d3614500c833c3972ed02
Nikola Vukovic <nikolav54@gmail.com>
2021-01-27 10:05:44 +0100

    Usluge style fix na Radio Button

--- src/components/Usluge/UslugeForm.js
+++ src/components/Usluge/UslugeForm.js
@@ -330,32 +330,35 @@ const UslugeForm = () => {
                     </div>
                     <div className="col-md-4">
                       <label className="form__label">Status</label>
-                      <div className="form__group">
-                        <div className="form__radio-group">
-                          {/* <RadioButton
+                      <div
+                        className="form__group"
+                        onChange={(event) => {
+                          console.log('event.target.value', event.target.value);
+                          values.status = event.target.value;
+                        }}
+                      >
+                        {/* <RadioButton
                             name="status"
                             label="Status"
                             options={statusOptions}
                           /> */}
-                          <div
-                            className="form__group"
-                            onChange={(event) => {
-                              console.log(
-                                'event.target.value',
-                                event.target.value
-                              );
-                              values.status = event.target.value;
-                            }}
+                        <div className="form__radio-group">
+                          <input type="radio" value="Aktivan" name="status" />
+                          <label
+                            htmlFor="Aktivan"
+                            className="form__radio-label"
                           >
-                            <input type="radio" value="Aktivan" name="status" />
                             Aktivan
-                            <input
-                              type="radio"
-                              value="Neaktivan"
-                              name="status"
-                            />
+                          </label>
+                        </div>
+                        <div className="form__radio-group">
+                          <input type="radio" value="Neaktivan" name="status" />
+                          <label
+                            htmlFor="Neaktivan"
+                            className="form__radio-label"
+                          >
                             Neaktivan
-                          </div>
+                          </label>
                         </div>
                       </div>
                     </div>
