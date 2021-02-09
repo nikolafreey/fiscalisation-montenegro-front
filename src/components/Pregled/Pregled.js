12c473701bc7b119c41d791a05ab6d2f255597b7
Nikola Vukovic <nikolav54@gmail.com>
2021-02-09 22:17:45 +0100

    Dashboard prvi dio

--- src/components/Pregled/Pregled.js
+++ src/components/Pregled/Pregled.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 const Pregled = () => {
   return (
-    <div>
-      Pregled
+    <div className="container">
+      <div className="row">
+        <div className="offset-md-1 offset-xl-3"></div>
+        <div className="col-md-11 col-xl-9">
+          <div className="dashboard">
+            <div className="dashboard-title">
+              <h1 className="heading-primary">Dobar dan!</h1>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Pregled
+export default Pregled;
