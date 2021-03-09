198d8b549b4a6fe28968c090dc596bf7667ec117
Nikola Vukovic <nikolav54@gmail.com>
2021-03-09 19:03:09 +0100

    Ogranicen na 2 decimale u tabeli racuni

--- src/components/Racuni/RacuniTableRow.js
+++ src/components/Racuni/RacuniTableRow.js
@@ -45,7 +45,7 @@ const RacuniTableRow = ({ item }) => {
 
   const currencyFormat = (num) => {
     // return '$' + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
-    return num.toString().replace('.', ',');
+    return Number(num).toFixed(2).replace('.', ',');
   };
 
   const handleClick = () => {
