2eb9580a1b5dd24e2d30336addb21431e3931c70
Nikola Vukovic <nikolav54@gmail.com>
2021-03-26 20:39:59 +0100

    Izmjene na validaciji i prevodima, izmejna dropdown komponente da podrzi prikazivanje validacije

--- src/validation/preduzeca.js
+++ src/validation/preduzeca.js
@@ -4,43 +4,32 @@ import $t from '../lang';
 export const PreduzecaSchema = Yup.object().shape({
   kratki_naziv: Yup.string()
     .min(3, $t('validacija.unosKratak'))
-    .max(50, $t('validacija.unosDugacak'))
+    .max(255, $t('validacija.unosDugacak'))
     .required($t('validacija.obavezno')),
   oblik_preduzeca: Yup.string()
     .min(3, $t('validacija.unosKratak'))
-    .max(50, $t('validacija.unosDugacak'))
+    .max(255, $t('validacija.unosDugacak'))
     .required($t('validacija.obavezno')),
   pib: Yup.string()
-    .min(3, $t('validacija.unosKratak'))
-    .max(50, $t('validacija.unosDugacak'))
+    .min(8, $t('validacija.unosKratak'))
+    .max(13, $t('validacija.unosDugacak'))
     .required($t('validacija.obavezno')),
   pdv: Yup.string()
     .min(13, $t('validacija.unosKratak'))
     .max(13, $t('validacija.unosDugacak'))
     .required($t('validacija.obavezno')),
-  twitter_username: Yup.string()
+  adresa: Yup.string()
     .min(3, $t('validacija.unosKratak'))
-    .max(100, $t('validacija.unosDugacak'))
+    .max(255, $t('validacija.unosDugacak'))
     .required($t('validacija.obavezno')),
-  instagram_username: Yup.string()
+  grad: Yup.string()
     .min(3, $t('validacija.unosKratak'))
-    .max(100, $t('validacija.unosDugacak'))
+    .max(255, $t('validacija.unosDugacak'))
     .required($t('validacija.obavezno')),
-  facebook_username: Yup.string()
+  drzava: Yup.string()
     .min(3, $t('validacija.unosKratak'))
-    .max(100, $t('validacija.unosDugacak'))
+    .max(255, $t('validacija.unosDugacak'))
     .required($t('validacija.obavezno')),
-  skype_username: Yup.string()
-    .min(3, $t('validacija.unosKratak'))
-    .max(100, $t('validacija.unosDugacak'))
-    .required($t('validacija.obavezno')),
-  telefon: Yup.string()
-    .min(3, $t('validacija.unosKratak'))
-    .max(50, $t('validacija.unosDugacak'))
-    .required($t('validacija.obavezno')),
-  email: Yup.string()
-    .min(3, $t('validacija.unosKratak'))
-    .max(50, $t('validacija.unosDugacak'))
-    .email($t('validacija.email'))
-    .required($t('validacija.obavezno')),
+  djelatnost: Yup.number().required($t('validacija.obavezno')),
+  kategorija: Yup.number().required($t('validacija.obavezno')),
 });
