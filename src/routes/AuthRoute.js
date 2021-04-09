cb2cce12882f227fa949ecb3dfd3b3986643fdd7
Nikola Vukovic <nikolav54@gmail.com>
2021-04-09 21:58:43 +0200

    Dodavanje tokena u LocalStorage i dodavanje u header kod svakog zahtjeva

--- src/routes/AuthRoute.js
+++ src/routes/AuthRoute.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { Redirect, Route } from "react-router-dom";
-import { authService } from "../services/AuthService";
-import { HOME } from "../constants/routes";
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { authService } from '../services/AuthService';
+import { HOME } from '../constants/routes';
 
 function AuthRoute({ children, ...props }) {
   if (authService.isAuthenticated()) return <Redirect to={HOME} />;
