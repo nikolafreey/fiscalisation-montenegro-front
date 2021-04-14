8e0bcd3573df95990353caf8fed8b84473e27677
Nikola Vukovic <nikolav54@gmail.com>
2021-04-14 20:05:16 +0200

    Validacija na preduzecima(kategorija_id) ispravljena

--- src/validation/preduzeca.js
+++ src/validation/preduzeca.js
@@ -15,7 +15,6 @@ export const PreduzecaSchema = Yup.object().shape({
     .max(13, $t('validacija.unosDugacak'))
     .required($t('preduzecaValidation.pib')),
   pdv: Yup.string()
-    .min(13, $t('validacija.unosKratak'))
     .max(13, $t('validacija.unosDugacak'))
     .required($t('preduzecaValidation.pdv')),
   adresa: Yup.string()
@@ -30,6 +29,6 @@ export const PreduzecaSchema = Yup.object().shape({
     .min(3, $t('validacija.unosKratak'))
     .max(255, $t('validacija.unosDugacak'))
     .required($t('preduzecaValidation.drzava')),
+  kategorija_id: Yup.number().required($t('preduzecaValidation.kategorija')),
   djelatnost: Yup.number().required($t('preduzecaValidation.djelatnost')),
-  kategorija_id: Yup.number().required($t('preduzecaValidation.kategorija_id')),
 });
