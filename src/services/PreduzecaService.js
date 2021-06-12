0083dd71d7d85145c84015d8de919ad8b1c981fa
Nikola Vukovic <nikolav54@gmail.com>
2021-06-12 17:39:16 +0200

    Izmjene za upload logotipa

--- src/services/PreduzecaService.js
+++ src/services/PreduzecaService.js
@@ -9,7 +9,24 @@ class PreduzecaService extends ApiService {
   getPreduzeca = (params) =>
     this.apiClient.get(ENDPOINTS.PREDUZECA, { params });
 
-  storePreduzece = (data) => this.apiClient.post(ENDPOINTS.PREDUZECA, data);
+  storePreduzece = (data) => {
+    console.log('data', data);
+    let ziroRacuni = JSON.stringify(data.ziro_racuni);
+    console.log('ziroRacuni', ziroRacuni);
+
+    let form_data = new FormData();
+    for (let key in data) {
+      form_data.append(key, data[key]);
+    }
+
+    form_data.append('ziro_racuni', JSON.stringify(data.ziro_racuni));
+
+    this.apiClient.post(ENDPOINTS.PREDUZECA, form_data, {
+      'Content-Type':
+        'multipart/form-data; charset=utf-8; boundary=' +
+        Math.random().toString().substr(2),
+    });
+  };
 
   getPreduzece = (id) =>
     this.apiClient.get(ENDPOINTS.PREDUZECE.replace('{id}', id));
