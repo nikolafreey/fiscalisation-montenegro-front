d4f5faf2131370876ab68462b16613974242cca0
Nikola Vukovic <nikolav54@gmail.com>
2021-06-21 09:45:22 +0200

    Usluga validacija naziva, validacija specijalnih karaktera

--- src/validation/usluga.js
+++ src/validation/usluga.js
@@ -4,7 +4,8 @@ import $t from '../lang';
 export const UslugeSchema = Yup.object().shape({
   naziv: Yup.string()
     .max(191, $t('validacija.unosDugacak'))
-    .required($t('uslugeValidation.naziv')),
+    .required($t('uslugeValidation.naziv'))
+    .matches(/^[aA-zZ0-9\s]+$/, $t('uslugeValidation.specialCharacters')),
   jedinica_mjere_id: Yup.number().required(
     $t('uslugeValidation.jedinicaMjere')
   ),
